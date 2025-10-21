db1734297d0ff0561bf642338a384a69242fe08c
ramanraj00 <r02519625@gmail.com>
2025-10-21 19:51:26 +0530

    SliderComponentDone

--- myapp/src/layout/ChampionsCardlayout.jsx
+++ myapp/src/layout/ChampionsCardlayout.jsx
@@ -2,23 +2,42 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ChampionsCard } from "../Data/ChampionsData";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Pagination,Mousewheel } from "swiper/modules";
  import "swiper/css/pagination";
+ import "swiper/css/mousewheel";
 import "swiper/css";
 
  function ChampionsCardSlider(){
     return (
         
         <div className="bg-gray-100 h-[500px] flex items-center justify-center">
+             <div className="w-full max-w-[1100px]">
             <Swiper 
-         modules={[Pagination]}
-         slidesPerView={1}
-         spaceBetween={20}
+           // onSwiper={(s) => (window.__sw = s)} to check everthing is true check in console
+         modules={[Pagination,Mousewheel ]}
+         slidesPerView={1.1}
+         spaceBetween={10}
          centeredSlides={true}
          grabCursor={true}
          allowTouchMove={true}
          simulateTouch={true}
+         touchRatio={1.2}
+         touchAngle={45}
+         mousewheel={{ forceToAxis: true, sensitivity: 0.8 }}
          pagination={{ clickable: true }}
+         
+       
+         // smoothness
+         
+freeModeMomentum={true}
+freeModeMomentumRatio={0.8}
+freeModeMomentumVelocityRatio={0.9}
+speed={600}
+autoHeight={true}
+
+
+
+
          >
 
             {ChampionsCard.map((card)=>(
@@ -33,7 +52,9 @@ import "swiper/css";
                 </SwiperSlide>
         ))}
       </Swiper>
-        </div>
+       </div>
+            </div>
+        
 
     );
 }
