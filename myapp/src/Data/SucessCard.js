7d562fd2f3f1d0051d4e25ca1ffd6fc53b3ba45c
ramanraj00 <r02519625@gmail.com>
2025-11-01 22:40:30 +0530

    Right Card section is done

--- myapp/src/Data/SucessCard.js
+++ myapp/src/Data/SucessCard.js
@@ -3,67 +3,67 @@ export const SucessCard = [
     {
         id:1,
         src:"https://asset.allen.in/969ed827-339b-4cbb-b121-930036ae2b1b/original.png?__ar__=0.722348",
-        label:"No shortcuts! From AIR 27,249 to AIR 1,341"
+        video:"/devd.mp4"
     },
 
      {
         id:2,
         src:"https://asset.allen.in/339a719a-253c-4c7c-9748-5010566f0f7b/original.png?__ar__=0.722348",
-        label:"From Bankura to a Govt. Medical College"
+        video:"/devd.mp4"
     },
 
      {
         id:3,
         src:"https://asset.allen.in/6fa4b8ee-9ff9-436d-8363-2a4e71e48b0a/original.png?__ar__=0.722348",
-        label:"AIR 29284 Then. AIR 2095 Now. My Comeback Story"
+        video:"/devd.mp4"
     },
 
      {
         id:4,
         src:"https://asset.allen.in/82c83836-c996-42c8-ab0e-13c452b8a04e/original.png?__ar__=0.722348",
-        label:"Laser sharp focus led him to success"
+        video:"/devd.mp4"
     },
 
      {
         id:5,
         src:"https://asset.allen.in/be27ab88-fa2c-4d17-8574-a7639a404720/original.png?__ar__=0.704225",
-        label:" Debater.ALLEN ONINEFlute player IlTian He did it all!"
+        video:"/devd.mp4"
     },
 
      {
         id:6,
         src:"https://asset.allen.in/6fd7257f-7ad8-4a72-85da-e10dafecb7a0/original.png?__ar__=0.704225",
-        label:"Never a topper kid e But now an IITian!"
+        video:"/devd.mp4"
     },
 
      {
         id:7,
         src:"https://asset.allen.in/ac8cf09d-48a3-437f-9c45-4da7835aeb7d/original.png?__ar__=0.704225",
-        label:"0% shortcut.100% discipline."
+        video:"/devd.mp4"
     },
 
      {
         id:8,
         src:"https://asset.allen.in/179e34a0-1b3b-4e2f-bfa4-0832f40b40d4/original.png?__ar__=0.722348",
-        label:"A surgeon in the making"
+        video:"/devd.mp4"
     },
 
      {
         id:9,
         src:"https://asset.allen.in/7c6815e3-d2a4-4160-a126-92368d68accd/original.png?__ar__=0.722348",
-        label:"  Maths lover urned NEET achiever    "
+        video:"/devd.mp4"
     },
 
      {
         id:10,
         src:"https://asset.allen.in/e090a069-29b3-4d41-bad2-67c4c20bc462/original.png?__ar__=0.722348",
-        label:"The forward thinking achiever"
+        video:"/devd.mp4"
     },
 
      {
         id:11,
         src:"https://asset.allen.in/193453f4-4c18-47be-9883-8ab424cfb18b/original.png?__ar__=0.722348",
-        label:"The online learning effect"
+         video:"/devd.mp4"
     },
 ]
 
--- myapp/src/layout/SucessStory.jsx
+++ myapp/src/layout/SucessStory.jsx
@@ -1,12 +1,18 @@
 import SucessCard from "../Data/SucessCard.js"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel } from "swiper/modules";
+import "swiper/css";
+import { useState } from "react";
 
 function SuccessStory () {
+
+   const [activeCard , setActiveCard] = useState(null);
     
     return (
 
-     <div className="flex">
+     <div className="flex items-center ">
 
-        <div>
+        <div className="mr-3">
 
         <div className=" text-center lg:text-left lg:ml-64 mt-8 ">
             <img
@@ -26,6 +32,85 @@ function SuccessStory () {
            <p>inspire us !</p>
         </div>
         </div>
+
+
+      <div className="flex justify-center mt-3">
+      <div className="w-full max-w-[900px]">
+
+        <Swiper
+        
+
+                  modules={[Mousewheel]}
+                        
+               slidesPerView={5}          
+                mousewheel={{ forceToAxis: true, sensitivity: 4 , thresholdDelta: 1, thresholdTime: 0 }}
+                speed={220}                   
+                centeredSlides={false}
+
+                loop={false}            
+                resistanceRatio={0.3}    
+                spaceBetween={14}
+
+ 
+
+
+               cssMode={true}
+
+
+                        
+        >
+
+      
+      {SucessCard.map(card => (
+     <SwiperSlide key={card.id} className="block w-full h-auto">
+      <div className=" overflow-hidden shadow">
+      <img onClick={()=>setActiveCard(card)} src={card.src} className="w-full h-auto" />
+      </div>
+      </SwiperSlide>
+
+     ))}
+      
+
+
+        </Swiper>
+
+      {activeCard && (
+         <div className="fixed inset-0 z-[9999] bg-black/50 flex items-center justify-center ">
+         
+            <div className="bg-black rounded-2xl shadow-2xl p-2  max-w-[600px] w-[90vw] relative">
+
+             <button
+            onClick={() => setActiveCard(null)}
+            aria-label="Close"
+            className=" rounded-full bg-black/70 text-white p-2 hover:bg-black focus:outline-none focus:ring-2 focus:ring-white/70 absolute -top-3 -right-3"
+             >
+             X
+        </button>
+
+
+
+            <div className=" rounded-xl  overflow-hidden aspect-video w-full "> 
+
+             <video src={activeCard.video} 
+             
+             controls
+             autoPlay
+             muted
+             playsInline
+             className="w-full h-full object-cover "
+             >
+
+             </video>
+            </div>
+            </div>
+
+            </div>
+       
+      )}
+
+
+      </div>
+      </div>   
 
 
         
